06557f4b36107e42cbb972ff126f812db9d65c1c
veer aryan <43904888+jeezburger@users.noreply.github.com>
2023-10-16 11:50:50 +0530

    Create typedef.cpp

---
+++ typedef.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+typedef int marks;
+typedef int roll; // typedef is merely used to change the name of datatype for
+                  // better readability
+int main() {
+  marks m1, m2;
+  roll r1, r2;
+  cin >> m1;
+  cout << m1 + 1;
+}
