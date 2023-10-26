d9d08d6f58e99f5d4663afedc2b0f009fde63548
veer aryan <43904888+jeezburger@users.noreply.github.com>
2023-10-27 01:15:41 +0530

    Create positivenegative.cpp

---
+++ positivenegative.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a;
+  cout << "enter a number ";
+  cin >> a;
+  if (a > 0) {
+    cout << "the number is positive";
+  } else {
+    cout << "the number is negative";
+  }
+  return 0;
+}
