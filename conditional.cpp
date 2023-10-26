f92333719dc2595577fd82ec874c3c05217e30cf
veer aryan <43904888+jeezburger@users.noreply.github.com>
2023-10-27 01:11:04 +0530

    Create conditional.cpp

---
+++ conditional.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  cout << "enter two numbers ";
+  cin >> a >> b;
+  if (a > b) {
+    cout << "" << a << " is the bigger number";
+
+  } else {
+    cout << "" << b << " is the bigger number";
+  }
+  return 0;
+}
